5ac6ea845cb882f7e2916b334bcc89eb999c0b8e
Muragijimana innocent <muragijimanainnocent3@gmail.com>
2023-02-03 08:30:27 +0200

    add user model

---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+
+const schema = new Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+schema.pre("save", async function (next) {
+  const user = this;
+  const hash = await bcrypt.hash(this.password, 10);
+
+  this.password = hash;
+  next();
+});
+
+UserSchema.methods.isValidPassword = async function (password) {
+  const user = this;
+  const compare = await bcrypt.compare(password, user.password);
+
+  return compare;
+};
+
+export default model("User", schema);
